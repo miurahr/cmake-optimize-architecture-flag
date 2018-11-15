f854229864d2a628e683058b8dd9e4b9f0ed5fac
Hiroshi Miura <miurahr@linux.com>
2018-11-15 11:09:33 +0900

    Replace internal macro by variable and foreach loop
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- OptimizeArchitectureFlags.cmake
+++ OptimizeArchitectureFlags.cmake
@@ -33,9 +33,7 @@ OptimizeArchitectureFlags
 
 .. note::
   Optimize_architecture_flags function defines two utility macro utilized inside
-  function to reduce code size:
-   _OFA_enable_or_disable() and _OFA_add_compiler_flag()
-
+  function to reduce code size: _OFA_add_compiler_flag()
 #]=======================================================================]
 
 
@@ -576,7 +574,34 @@ Other supported values are: \"none\", \"generic\", \"core\", \"merom\" (65nm Cor
                 endif()
             endif()
 
-            macro(_OFA_enable_or_disable _name _flag _documentation _broken)
+            #set values _name _flag _documentation _broken
+            set(SSE2 "sse2" "Use SSE2. If SSE2 instructions are not enabled the SSE implementation will be disabled." false)
+            set(SSE3 "sse3" "Use SSE3. If SSE3 instructions are not enabled they will be emulated." false)
+            set(SSSE3 "ssse3" "Use SSSE3. If SSSE3 instructions are not enabled they will be emulated." false)
+            set(SSE4_1 "sse4.1" "Use SSE4.1. If SSE4.1 instructions are not enabled they will be emulated." false)
+            set(SSE4_2 "sse4.2" "Use SSE4.2. If SSE4.2 instructions are not enabled they will be emulated." false)
+            set(SSE4a "sse4a" "Use SSE4a. If SSE4a instructions are not enabled they will be emulated." false)
+            set(AVX "avx" "Use AVX. This will all floating-point vector sizes relative to SSE." _avx_broken)
+            set(FMA "fma" "Use FMA." _avx_broken)
+            set(BMI2 "bmi2" "Use BMI2." _avx_broken)
+            set(AVX2 "avx2" "Use AVX2. This will double all of the vector sizes relative to SSE." _avx2_broken)
+            set(XOP "xop" "Use XOP." _xop_broken)
+            set(FMA4 "fma4" "Use FMA4." _fma4_broken)
+            set(AVX512F "avx512f" "Use AVX512F. This will double all floating-point vector sizes relative to AVX2." false)
+            set(AVX512VL "avx512vl" "Use AVX512VL. This enables 128- and 256-bit vector length instructions with EVEX coding (improved write-masking & more vector registers)." _avx2_broken)
+            set(AVX512PF "avx512pf" "Use AVX512PF. This enables prefetch instructions for gathers and scatters." false)
+            set(AVX512ER "avx512er" "Use AVX512ER. This enables exponential and reciprocal instructions." false)
+            set(AVX512CD "avx512cd" "Use AVX512CD." false)
+            set(AVX512DQ "avx512dq" "Use AVX512DQ." false)
+            set(AVX512BW "avx512bw" "Use AVX512BW." false)
+            set(AVX512IFMA "avx512ifma" "Use AVX512IFMA." false)
+            set(AVX512VBMI "avx512vbmi" "Use AVX512VBMI." false)
+            foreach(target IN ITEMS SSE2 SSE3 SSSE3 SSE4_1 SSE4_2 SSE4a AVX FMA BMI2 AVX2 XOP FMA4
+                    AVX512F AVX512VL AVX512PF AVX512ER AVX512CD AVX512DQ AVX512BW AVX512IFMA AVX512VBMI)
+                set(_name ${target})
+                list(GET ${target} 0 _flag)
+                list(GET ${target} 1 _documentation)
+                list(GET ${target} 2 _broken)
                 list(FIND _available_vector_units_list "${_flag}" _found)
                 if(_broken OR (_found EQUAL -1))
                     set(USE_${_name} FALSE CACHE BOOL "${documentation}" ${_force})
@@ -589,28 +614,8 @@ Other supported values are: \"none\", \"generic\", \"core\", \"merom\" (65nm Cor
                 else()
                     list(APPEND _disable_vector_unit_list "${_flag}")
                 endif()
-            endmacro()
-            _OFA_enable_or_disable(SSE2 "sse2" "Use SSE2. If SSE2 instructions are not enabled the SSE implementation will be disabled." false)
-            _OFA_enable_or_disable(SSE3 "sse3" "Use SSE3. If SSE3 instructions are not enabled they will be emulated." false)
-            _OFA_enable_or_disable(SSSE3 "ssse3" "Use SSSE3. If SSSE3 instructions are not enabled they will be emulated." false)
-            _OFA_enable_or_disable(SSE4_1 "sse4.1" "Use SSE4.1. If SSE4.1 instructions are not enabled they will be emulated." false)
-            _OFA_enable_or_disable(SSE4_2 "sse4.2" "Use SSE4.2. If SSE4.2 instructions are not enabled they will be emulated." false)
-            _OFA_enable_or_disable(SSE4a "sse4a" "Use SSE4a. If SSE4a instructions are not enabled they will be emulated." false)
-            _OFA_enable_or_disable(AVX "avx" "Use AVX. This will all floating-point vector sizes relative to SSE." _avx_broken)
-            _OFA_enable_or_disable(FMA "fma" "Use FMA." _avx_broken)
-            _OFA_enable_or_disable(BMI2 "bmi2" "Use BMI2." _avx_broken)
-            _OFA_enable_or_disable(AVX2 "avx2" "Use AVX2. This will double all of the vector sizes relative to SSE." _avx2_broken)
-            _OFA_enable_or_disable(XOP "xop" "Use XOP." _xop_broken)
-            _OFA_enable_or_disable(FMA4 "fma4" "Use FMA4." _fma4_broken)
-            _OFA_enable_or_disable(AVX512F "avx512f" "Use AVX512F. This will double all floating-point vector sizes relative to AVX2." false)
-            _OFA_enable_or_disable(AVX512VL "avx512vl" "Use AVX512VL. This enables 128- and 256-bit vector length instructions with EVEX coding (improved write-masking & more vector registers)." _avx2_broken)
-            _OFA_enable_or_disable(AVX512PF "avx512pf" "Use AVX512PF. This enables prefetch instructions for gathers and scatters." false)
-            _OFA_enable_or_disable(AVX512ER "avx512er" "Use AVX512ER. This enables exponential and reciprocal instructions." false)
-            _OFA_enable_or_disable(AVX512CD "avx512cd" "Use AVX512CD." false)
-            _OFA_enable_or_disable(AVX512DQ "avx512dq" "Use AVX512DQ." false)
-            _OFA_enable_or_disable(AVX512BW "avx512bw" "Use AVX512BW." false)
-            _OFA_enable_or_disable(AVX512IFMA "avx512ifma" "Use AVX512IFMA." false)
-            _OFA_enable_or_disable(AVX512VBMI "avx512vbmi" "Use AVX512VBMI." false)
+            endforeach()
+
 
             if(MSVC)
                 # MSVC on 32 bit can select /arch:SSE2 (since 2010 also /arch:AVX)
